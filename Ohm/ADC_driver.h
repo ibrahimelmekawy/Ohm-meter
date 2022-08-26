fd0961a8972be0dda5bc073da4364e138a9ab675
Ibrahim Elmekawy <87153297+ibrahimelmekawy@users.noreply.github.com>
2022-08-26 14:49:32 +0200

    Add files via upload

---
+++ Ohm/ADC_driver.h
@@ -0,0 +1,16 @@
+/*
+ * ADC_driver.h
+ *
+ * Created: 17-May-22 7:46:23 PM
+ *  Author: IbrahimElmekawy
+ */ 
+
+
+#ifndef ADC_DRIVER_H_
+#define ADC_DRIVER_H_
+void ADC_vinit(void);
+unsigned short ADC_u16Read(void);
+
+
+
+#endif /* ADC_DRIVER_H_ */
